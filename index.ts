4cfa01df46145d083cfe79cb3335375d4f3fc67a
JohnsCoder <joaopdossantos1515@gmail.com>
2022-07-26 12:28:44 -0300

    finishing project

--- index.ts
+++ index.ts
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import auth from "./routes/auth";
-import todo from './routes/todo'
-const port = 8000;
+import todo from "./routes/todo";
 const app = express();
 
 app.use(express.urlencoded());
@@ -11,4 +10,6 @@ app.use(cors());
 app.use("/auth", auth);
 app.use("/todos", todo);
 
-app.listen(port, () => console.log(`app running on http://localhost:${port}`));
+app.listen(process.env.PORT || 8000, () =>
+  console.log("Your Application is running")
+);
