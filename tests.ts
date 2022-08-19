715b0cf5bcda675ddaf98c8ea08d7494b51a647f
JohnsCoder <joaopdossantos1515@gmail.com>
2022-08-19 12:48:06 -0300

    removing file

--- tests.ts
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const id = jwt.verify(
-     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODAsImlhdCI6MTY1ODc2NDgzOSwiZXhwIjoxNjU4ODUxMjM5fQ.wK8yAtuCOCshqJCzfpqKhsAiRgSysM8HQeed5mqAPGU',
-     '12345678945867325478597642389563'
-   )
-
-   console.log(id)
